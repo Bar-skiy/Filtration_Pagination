92d27746035e00df39ad0010b499857eddbd9b3e
Bar-skiy <vbar7575@gmail.com>
2022-11-11 17:30:22 +0300

    formating after action

--- src/components/request/RequestTable.vue
+++ src/components/request/RequestTable.vue
@@ -1,64 +1,64 @@
 <template>
   <div>
-    <h4 v-if="requests.length === 0"
-        class="text-center">
-      Данных пока нет
-    </h4>
+    <h4 v-if="requests.length === 0" class="text-center">Данных пока нет</h4>
     <table v-else class="table">
       <thead>
-      <tr>
-        <th>Дата</th>
-        <th>Название</th>
-        <th>Количество</th>
-        <th>Расстояние</th>
-      </tr>
+        <tr>
+          <th>Дата</th>
+          <th>Название</th>
+          <th>Количество</th>
+          <th>Расстояние</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="r in paginatedData" :key="r.id">
-        <td>{{ new Date(r.date).toLocaleDateString() }}</td>
-        <td>{{ r.designation }}</td>
-        <td>{{ r.amount }}</td>
-        <td>{{ r.distance }}</td>
-      </tr>
-
+        <tr v-for="r in paginatedData" :key="r.id">
+          <td>{{ new Date(r.date).toLocaleDateString() }}</td>
+          <td>{{ r.designation }}</td>
+          <td>{{ r.amount }}</td>
+          <td>{{ r.distance }}</td>
+        </tr>
       </tbody>
     </table>
 
     <div class="card-title">
       <div>
-        <button class="btn warning"
-                :disabled="pageNumber === 0"
-                @click="prevPage">назад
+        <button
+          class="btn warning"
+          :disabled="pageNumber === 0"
+          @click="prevPage"
+        >
+          назад
         </button>
 
-        <button class="btn sir"
-                :disabled="pageNumber >= pageCount -1"
-                @click="nextPage">вперед
+        <button
+          class="btn sir"
+          :disabled="pageNumber >= pageCount - 1"
+          @click="nextPage"
+        >
+          вперед
         </button>
       </div>
 
-      <h5 class="pg"><b> Страница  {{ pageNumber + 1 }} из {{ pageCount }} </b></h5>
+      <h5 class="pg">
+        <b> Страница {{ pageNumber + 1 }} из {{ pageCount }} </b>
+      </h5>
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
-
   data() {
     return {
       pageNumber: 0,
-      size: 11
+      size: 11,
     };
-
   },
   props: {
     requests: {
       type: Array,
-      required: true
+      required: true,
     },
-
   },
   methods: {
     nextPage() {
@@ -66,21 +66,19 @@ export default {
     },
     prevPage() {
       this.pageNumber--;
-    }
+    },
   },
-
   computed: {
     pageCount() {
       let l = this.requests.length,
-          s = this.size;
+        s = this.size;
       return Math.ceil(l / s);
     },
     paginatedData() {
       const start = this.pageNumber * this.size,
-          end = start + this.size;
+        end = start + this.size;
       return this.requests.slice(start, end);
-    }
+    },
   },
-
-}
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,56 @@
-import {createStore, createLogger} from 'vuex'
+import {createStore, createLogger} from "vuex";
 
 export default createStore({
-    plugins: [createLogger()],
+  plugins: [createLogger()],
 
-    state: {
-        date: null,
-        designation: '',
-        amount: '',
-        distance: '',
-        requests: [],
-        requestDB: {},
+  state: {
+    date: null,
+    designation: "",
+    amount: "",
+    distance: "",
+    requests: [],
+    requestDB: {},
+  },
+  mutations: {
+    // комментарии: создание объекта из данных модального окна
+    createRequest(state) {
+      state.requestDB = {
+        date: new Date(date.value).setHours(23, 59, 59, 999),
+        designation: designation.value,
+        amount: amount.value,
+        distance: distance.value,
+      };
+      state.requests.push(state.requestDB);
     },
-    mutations: {
-        // комментарии: создание объекта из данных модального окна
-        createRequest(state) {
-            state.requestDB = {
-                date: new Date(date.value).setHours(23, 59, 59, 999),
-                designation: designation.value,
-                amount: amount.value,
-                distance: distance.value,
-            }
-            state.requests.push(state.requestDB)
-        },
-    },
-    actions: {
-         // комментарии: загрузка данных в BD
-         async submitRequestDB(context) {
-            await fetch('https://welbex-test-40cd7-default-rtdb.firebaseio.com//requests.json', {
-               method: 'POST',
-               headers: {
-                   'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(
-                    context.state.requestDB
-               )
-           })
-       },
-        // комментарии: загрузка данных из BD при перезагрузке страниц
-        async loadDataDB(context) {
-            const result = await fetch('https://welbex-test-40cd7-default-rtdb.firebaseio.com//requests.json')
-            const db = await result.json()
-            // комментарии: пересборка массива данных для vuex
-            context.state.requests = Object.keys(db).map(id => ({ ...db[id], id }))
-        },
-    },
-    modules: {},
-    getters: {
-        requests(state) {
-            return state.requests
+  },
+  actions: {
+    // комментарии: загрузка данных в BD
+    async submitRequestDB(context) {
+      await fetch(
+        "https://welbex-test-40cd7-default-rtdb.firebaseio.com//requests.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(context.state.requestDB),
         }
-    }
-})
+      );
+    },
+    // комментарии: загрузка данных из BD при перезагрузке страниц
+    async loadDataDB(context) {
+      const result = await fetch(
+        "https://welbex-test-40cd7-default-rtdb.firebaseio.com//requests.json"
+      );
+      const db = await result.json();
+      // комментарии: пересборка массива данных для vuex
+      context.state.requests = Object.keys(db).map((id) => ({...db[id], id}));
+    },
+  },
+  modules: {},
+  getters: {
+    requests(state) {
+      return state.requests;
+    },
+  },
+});
